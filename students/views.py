47975244fd3a6a1b1aebfdcbdf1df9e36b9f83aa
Alexanader <berestok6797@gmail.com>
2018-12-13 23:45:55 +0200

    statc dir

--- students/views.py
+++ students/views.py
@@ -1,1 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# Views for Group
+def students_list(request):
+    return render(request,'students/students_list.html', {})
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+def students_add(request,):
+    return HttpResponse('<h1>Add Student %s</h1>')
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+# Views for Group
+def groups_list(request):
+    return render(request,'<h1>Groups Listing</h1>')
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+def groups_add(request):
+    return HttpResponse('<h1>Add Group %s</h1>')
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
